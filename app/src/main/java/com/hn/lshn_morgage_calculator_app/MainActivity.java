819887ec42b513833ca0549a2679b9ff92c0fef8
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2019-02-19 13:46:08 -0500

    Init project

---
+++ app/src/main/java/com/hn/lshn_morgage_calculator_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hn.lshn_morgage_calculator_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
